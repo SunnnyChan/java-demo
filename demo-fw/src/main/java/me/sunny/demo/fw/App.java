f3e05493c37a9f0148c78d7f4910669ef61d293a
sunnychan <sunnnychan@gmail.com>
2019-07-01 14:56:29 +0800

    sunnychan

---
+++ demo-fw/src/main/java/me/sunny/demo/fw/App.java
@@ -0,0 +1,13 @@
+package me.sunny.demo.fw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ demo-network/src/main/java/me/sunny/demo/network/App.java
@@ -0,0 +1,13 @@
+package me.sunny.demo.network;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
