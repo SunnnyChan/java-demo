debe9bc2f886e449795cdb943348d57ce4dff145
sunnychan <sunnnychan@gmail.com>
2019-07-31 11:17:45 +0800

    sunnychan

---
+++ demo-spring/src/main/java/me/sunny/demo/spring/trans/Trans.java
@@ -0,0 +1,8 @@
+package me.sunny.demo.spring.trans;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public class Trans {
+
+}
---
+++ demo-spring/src/main/java/me/sunny/demo/spring/trans/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring 事务
+ */
+package me.sunny.demo.spring.trans;
