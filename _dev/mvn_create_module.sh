e3b31a65a050d0cf4a817b00699d656cc6d0bc25
sunnychan <sunnnychan@gmail.com>
2020-03-28 17:57:41 +0800

    sunnychan

--- _dev/mvn_create_module.sh
+++ _dev/mvn_create_module.sh
@@ -4,3 +4,5 @@ PROJECT_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd )/.."
 
 cd ${PROJECT_DIR} && mvn archetype:generate -DgroupId=$1 -DartifactId=$2 \
 -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+
---
+++ demo-basics/src/main/java/me/sunny/demo/basics/TypeCast.java
@@ -0,0 +1,8 @@
+package me.sunny.demo.basics;
+
+public class TypeCast {
+
+    public String booleanToString(boolean b) {
+        return String.valueOf(b);
+    }
+}
