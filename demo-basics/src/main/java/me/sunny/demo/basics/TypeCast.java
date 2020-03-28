66372af0d057ca3777d77fb2ef613c103f9c8cdd
sunnychan <sunnnychan@gmail.com>
2020-03-28 18:11:57 +0800

    sunnychan

--- demo-basics/src/main/java/me/sunny/demo/basics/TypeCast.java
+++ demo-basics/src/main/java/me/sunny/demo/basics/TypeCast.java
@@ -2,6 +2,10 @@ package me.sunny.demo.basics;
 
 public class TypeCast {
 
+    public String wrapperBooleanToString(Boolean b) {
+        return String.valueOf(b);
+    }
+
     public String booleanToString(boolean b) {
         return String.valueOf(b);
     }
