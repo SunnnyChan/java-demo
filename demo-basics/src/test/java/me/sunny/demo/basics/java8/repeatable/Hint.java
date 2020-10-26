0b63d848e9483a7fe1b50ccabbc7cb11b040ea50
sunnychan <sunnnychan@gmail.com>
2020-10-26 15:38:03 +0800

    sunnychan

---
+++ demo-basics/src/test/java/me/sunny/demo/basics/java8/repeatable/Hint.java
@@ -0,0 +1,14 @@
+package me.sunny.demo.basics.java8.repeatable;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(Hints.class)
+public @interface Hint {
+  String value();
+}
